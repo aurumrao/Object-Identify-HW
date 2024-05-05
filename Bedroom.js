48178c0b17c93bbaaa6b8b68d10b19d58003d49f
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-05-05 17:07:51 -0500

    Add files via upload

---
+++ Bedroom.js
@@ -0,0 +1,25 @@
+status = "";
+image = "";
+function preload(){
+    image = loadImage("Bedroom.jpg");
+}
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Cocossd Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
